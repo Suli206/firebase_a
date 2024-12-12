3c1b98c7f1c97b904ae11ca2c8bafd428f8d3aa0
Suli206 <sayilchiev@gmail.com>
2024-12-12 20:22:17 +0600

    add

--- lib/features/auth/Sing Up/sing_up.dart
+++ lib/features/auth/Sing Up/sing_up.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_a/features/auth/Sing%20In/sing_in.dart';
 import 'package:firebase_a/features/auth/Sing%20Up/cubit/up_singl_cubit.dart';
+import 'package:firebase_a/features/navigator_sreen/navi_screen.dart';
 import 'package:firebase_a/features/notes/notes_home.dart';
 import 'package:firebase_a/theme/app_text_styles.dart';
 import 'package:firebase_a/widget/custom_text_fields.dart';
@@ -24,146 +25,162 @@ class _SingUpSreenState extends State<SingUpSreen> {
     return BlocProvider(
       create: (context) => UpSinglCubit(),
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Register Account',
-            style: AppTextStyles.s24W400(),
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 26),
-                Text(
-                  'Email Address',
-                  style: AppTextStyles.s18W500(),
-                ),
-                CustomTextField(
-                  controller: controllerEmailUp,
-                  errorText: errorTextEmail,
-                ),
-                const SizedBox(height: 26),
-                Text(
-                  'Password',
-                  style: AppTextStyles.s18W500(),
-                ),
-                CustomTextField(
-                  controller: controllerPasswordUp,
-                  errorText: errorTextPassword,
-                ),
-                const SizedBox(height: 26),
-                BlocListener<UpSinglCubit, UpSinglState>(
-                  child: Center(
-                    child: SizedBox(
-                      width: 280,
-                      height: 45,
-                      child: Builder(builder: (context) {
-                        return ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              // 1 CustomTextField
-                              if (controllerEmailUp.text.isEmpty) {
-                                errorTextEmail = '';
-                              } else {
-                                if (controllerEmailUp.text.isNotEmpty &&
-                                    controllerEmailUp.text.length > 5) {
-                                  if (controllerEmailUp.text
-                                      .contains('@gmail.com')) {
-                                    errorTextEmail = '';
-                                    // 2 CustomTextField
-                                    if (controllerPasswordUp.text.isEmpty) {
-                                      errorTextPassword = '';
+        body: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/images/sing_up.jpg',
+                fit: BoxFit.cover,
+                height: 900,
+              ),
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Register Account',
+                          style: AppTextStyles.s24W400(),
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      Text(
+                        'Email Address',
+                        style: AppTextStyles.s18W500(),
+                      ),
+                      CustomTextField(
+                        controller: controllerEmailUp,
+                        errorText: errorTextEmail,
+                      ),
+                      const SizedBox(height: 26),
+                      Text(
+                        'Password',
+                        style: AppTextStyles.s18W500(),
+                      ),
+                      CustomTextField(
+                        controller: controllerPasswordUp,
+                        errorText: errorTextPassword,
+                      ),
+                      const SizedBox(height: 26),
+                      BlocListener<UpSinglCubit, UpSinglState>(
+                        child: Center(
+                          child: SizedBox(
+                            width: 280,
+                            height: 45,
+                            child: Builder(builder: (context) {
+                              return ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    // 1 CustomTextField
+                                    if (controllerEmailUp.text.isEmpty) {
+                                      errorTextEmail = '';
                                     } else {
-                                      if (controllerPasswordUp
-                                              .text.isNotEmpty &&
-                                          controllerPasswordUp.text.length >
-                                              5) {
-                                        errorTextPassword = '';
-                                        // UpSinglCubit
-                                        context.read<UpSinglCubit>().singlup(
-                                              controllerEmailUp.text
-                                                  .toLowerCase(),
-                                              controllerPasswordUp.text,
-                                            );
+                                      if (controllerEmailUp.text.isNotEmpty &&
+                                          controllerEmailUp.text.length > 5) {
+                                        if (controllerEmailUp.text
+                                            .contains('@gmail.com')) {
+                                          errorTextEmail = '';
+                                          // 2 CustomTextField
+                                          if (controllerPasswordUp
+                                              .text.isEmpty) {
+                                            errorTextPassword = '';
+                                          } else {
+                                            if (controllerPasswordUp
+                                                    .text.isNotEmpty &&
+                                                controllerPasswordUp
+                                                        .text.length >
+                                                    5) {
+                                              errorTextPassword = '';
+                                              // UpSinglCubit
+                                              context
+                                                  .read<UpSinglCubit>()
+                                                  .singlup(
+                                                    controllerEmailUp.text
+                                                        .toLowerCase(),
+                                                    controllerPasswordUp.text,
+                                                  );
+                                            } else {
+                                              errorTextPassword =
+                                                  'Не должно быть меньше 5';
+                                            }
+                                          }
+                                          //
+                                        } else {
+                                          errorTextEmail = 'Bведите @gmail.com';
+                                        }
                                       } else {
-                                        errorTextPassword =
+                                        errorTextEmail =
                                             'Не должно быть меньше 5';
                                       }
                                     }
                                     //
-                                  } else {
-                                    errorTextEmail = 'Bведите @gmail.com';
-                                  }
-                                } else {
-                                  errorTextEmail = 'Не должно быть меньше 5';
-                                }
-                              }
-                              //
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue),
-                          child: Text(
-                            'Sing Up',
-                            style: AppTextStyles.s14W400(),
+                                  });
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.blue),
+                                child: Text(
+                                  'Sing Up',
+                                  style: AppTextStyles.s14W400(),
+                                ),
+                              );
+                            }),
                           ),
-                        );
-                      }),
-                    ),
-                  ),
-                  listener: (context, state) => state.whenOrNull(
-                    success: () {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const NotesHome(),
                         ),
-                        (_) => false,
-                      );
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                            content: Text('Успешно зарегистрирован')),
-                      );
-                      return null;
-                    },
-                    error: (error) {
-                      return ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text(error)),
-                      );
-                    },
+                        listener: (context, state) => state.whenOrNull(
+                          success: () {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const NaviScreen(),
+                              ),
+                              (_) => false,
+                            );
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('Successfully registered')),
+                            );
+                            return null;
+                          },
+                          error: (error) {
+                            return ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text(error)),
+                            );
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 390),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Already have account?',
+                            style: AppTextStyles.s14W400(
+                              color: Colors.white.withOpacity(0.6),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SingInSreen(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Login In',
+                              style: AppTextStyles.s14W400(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 390),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Already have account?',
-                      style: AppTextStyles.s14W400(
-                        color: Colors.black.withOpacity(0.5),
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SingInSreen(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'Login In',
-                        style: AppTextStyles.s14W400(color: Colors.black),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/theme/app_text_styles.dart
+++ lib/theme/app_text_styles.dart
@@ -5,7 +5,7 @@ abstract class AppTextStyles {
         color: color ?? Colors.white,
         fontSize: 14,
         height: 0.8,
-        fontWeight: FontWeight.w400,
+        fontWeight: FontWeight.w600,
       );
   static TextStyle s18W500({Color? color}) => TextStyle(
         color: color ?? Colors.black,
@@ -20,6 +20,7 @@ abstract class AppTextStyles {
         fontSize: 22,
         fontWeight: FontWeight.w500,
       );
+
   static TextStyle s24W400({Color? color}) => TextStyle(
         color: color ?? Colors.black,
         fontSize: 24,
